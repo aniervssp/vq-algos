24bb2276b53fe56041ebd570ea144c98170caedf
Anier Velasco <anier.velasco@strategicplatform.com>
2026-09-03 14:20:12 +0200

    refactor eval_quant_model to improve MSE distortion evaluation and update plotting labels

--- evals.py
+++ evals.py
@@ -1,11 +1,12 @@
 import polars as pl
 import pandas as pd
 import numpy as np
-from quantizers import TurboQuantMSE, TurboQuantProd
+from quantizers import TurboQuantMSE
 from metrics import compute_mse
 from datasets import load_dataset
 import matplotlib.pyplot as plt
 import math 
+import warnings
 
 def eval_quant_model(X, bit_widths, model, lowerbound_mse, upperbound_mse):
     d = X.shape[-1]
@@ -14,23 +15,31 @@ def eval_quant_model(X, bit_widths, model, lowerbound_mse, upperbound_mse):
     for b in bit_widths:
         print(f"Evaluating QuantMSE for bit-width={b}...")
         quant_mse = model(dim=d, bit_width=b)
-        idx = quant_mse.quantize(sample_emb)
-        sample_emb_approx = quant_mse.dequantize(idx)
-        mse = compute_mse(sample_emb, sample_emb_approx)
+        idx = quant_mse.quantize(X)
+
+        X_approx = quant_mse.dequantize(idx)
+        mse = compute_mse(X, X_approx)
         mse_distortions.append(mse)
 
-    plt.plot(bit_widths, mse_distortions, label="empirical mse distortions")
+    plt.plot(
+        bit_widths,
+        mse_distortions,
+        label="empirical"
+    )
     plt.plot(
         bit_widths,
         upperbound_mse(bit_widths),
-        label="theoretical upper bound for the mse distortion",
+        label="theoretical upper bound",
     )
     plt.plot(
         bit_widths,
         lowerbound_mse(bit_widths),
-        label="theoretical lower bound for the mse distortion",
+        label="theoretical lower bound",
     )
     plt.legend()
+    plt.xlabel("Bit-widths")
+    plt.ylabel("MSE Distortions")
+    plt.title("MSE Distortions vs Bit-widths")
     plt.savefig("eval_quant_mse.png")
 
 def load_data():
@@ -56,14 +65,16 @@ def load_data():
 
     return sample_emb
 
-if __name__ == "__main__":
-    
+def main():
     sample_emb = load_data()
-    bit_widths = np.array([1, 2, 3, 4, 8])
+    bit_widths = np.array([1, 2, 3, 4, 5, 7])
 
     lowerbound_mse = lambda b : 1/ (4**b)
     upperbound_mse = lambda b : math.sqrt(3) * math.pi / (2 * (4**b))
     quant_model = TurboQuantMSE
 
-    eval_quant_model(sample_emb, bit_widths, quant_model, lowerbound_mse, upperbound_mse)
+    eval_quant_model(sample_emb, bit_widths, quant_model, lowerbound_mse, upperbound_mse)    
 
+if __name__ == "__main__":
+    main()
+    
